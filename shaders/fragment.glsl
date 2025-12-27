e8f7d277becaed42edad0a06e0c3338cf89d5d6c
Ravi Sinha <ravisinha@my.unt.edu>
2025-12-27 14:10:52 -0600

    Add first shaders

---
+++ shaders/fragment.glsl
@@ -0,0 +1,8 @@
+#version 330 core
+
+out vec4 FragColor;
+
+void main() {
+    // For now, just output solid cyan
+    FragColor = vec4(0.0, 0.8, 0.8, 1.0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,35 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h> // Glew must be added before OpenGL headers
 #include <OpenGL/gl.h>  // macOS OpenGL
+#include <fstream>
+#include <sstream>
+#include <string>
+
+// Helper function to load shader source from file
+std::string loadShaderSource(const char* filepath) {
+    std::ifstream file(filepath);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+
+// Helper function to compile a shader
+GLuint compileShader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    // Check for errors
+    GLint success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cerr << "Shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+    return shader;
+}
 
 int main(int argc, char* argv[]) {
     // Initialize SDL
@@ -67,6 +96,56 @@ int main(int argc, char* argv[]) {
     -1.0f,  1.0f   // Top-left
     };
 
+    // Upload to GPU
+    // Create Vertex Array Object (VAO)
+    // This remembers the configuration of how to interpret vertex data
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // Bind the VAO first - everything we do now gets recorded in it
+    glBindVertexArray(VAO);
+
+    // Upload vertex data to GPU
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Tell OpenGL how to interpret the data
+    // "Position attribute is at location 0, has 2 components (x,y), type float"
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Unbind (good practice)
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Load shader sources
+    std::string vertSource = loadShaderSource("../shaders/vertex.glsl");
+    std::string fragSource = loadShaderSource("../shaders/fragment.glsl");
+
+    // Compile shaders
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertSource.c_str());
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragSource.c_str());
+
+    // Link into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check linking errors
+    GLint success;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Shader linking failed:\n" << infoLog << std::endl;
+    }
+
+    // Clean up individual shaders (we don't need them after linking)
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     // Main loop flag
     bool running = true;
     SDL_Event event;
@@ -83,18 +162,28 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // Clear to a nice cyberpunk magenta
-        // Cyan: (0.0f, 0.8f, 0.8f, 1.0f)
-        // Neon pink: (1.0f, 0.0f, 0.5f, 1.0f)
-        // Deep blue: (0.0f, 0.0f, 0.2f, 1.0f)
-        glClearColor(0.0f, 0.9f, 0.8f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use our shader program
+        glUseProgram(shaderProgram);
+
+        // Draw the quad
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);  // 6 vertices = 2 triangles
+        glBindVertexArray(0);
+
+        SDL_GL_SwapWindow(window);
 
         // Swap buffers (double buffering)
         SDL_GL_SwapWindow(window);
 
         SDL_Delay(16); // ~60 FPS
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // Cleanup
     SDL_GL_DeleteContext(glContext);
